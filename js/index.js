c50c4d056edbd7e9cc02444e1f18e798e7fce631
Sreejan Naru <sreejannaru34@gmail.com>
2022-11-20 09:42:08 +0530

    Layout Fixed

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// ================ Scroll-up Button ================
+
+const scrollBtn = document.getElementById('scroll-up');
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
